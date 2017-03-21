d21cae3df11c4b8c076fd9f63acd94b8dca5a548
moseleygj <moseleygj@gmail.com>
2017-03-20 23:55:01 -0400

    Update users.sh

--- users.sh
+++ users.sh
@@ -7,13 +7,21 @@
 #Enter a username 
 echo "Enter a new user name, followed by [ENTER]:"
 read name
-  
- #check if username exist
- awk -F':' '{ print $1}' /etc/passwd #list usernames in /etc/passwd
+#check if empty
+  if [$name = ""];then
+  echo "username is NOT valid.";
+  else
+  echo "username is good.";
+  fi
+ #check if username exist already
+ # $(awk -F':' '{ print $1}' );
+ 
+ user_exists=$(id -u user > /dev/null 2>&1; echo $?) 
+ 
+ echo $user_exists;
  
  #create user
  
  #group
  
- #rights
- 
+ #rights 
