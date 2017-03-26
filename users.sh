edc2d7aea3a483e24e32ed67dae1385d211dceb0
moseleygj <moseleygj@gmail.com>
2017-03-26 11:28:39 -0400

    Update users.sh

--- users.sh
+++ users.sh
@@ -1,15 +1,16 @@
+
 #!/usr/bin/bash
 #Name:		Linux Automation project
 #purpose:	Add User Account
 #author:	Gordon Moseley
 #-------------------------------------------------------------------
-PATH="$PATH":/usr/bin/
 #Enter a username 
 clear
-echo "Enter a new user name, followed by [ENTER]:"
-read name;
+
+echo "Enter a new user name, followed by [ENTER]:";
+read name
 #check if empty
-  if [$name -eq ""];then
+  if [ $name = "" ];then
   echo "username \"$name\" is NOT valid[EMPTY].";
   else
   echo "username  \"$name\" is valid.";
@@ -18,15 +19,14 @@ read name;
  # $(awk -F':' '{ print $1}' );
  
 user_exists=$(id -u "$name" > /dev/null 2>&1; echo $?); 
- if [$user_exists -eq "0"]; then
- echo "User name  \"$name\" : AVAILIBLE.";
- else
+ if [ $user_exists -eq "0" ]; then
  echo "User name  \"$name\" : NOT AVAILIBLE.";
+ else
+ echo "User name  \"$name\" :  AVAILIBLE.";
  fi
- echo $user_exists;
  exit 0
  #create user
  
  #group
  
- #rights 
+#rights 
