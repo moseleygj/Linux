fded80fd50277f2990fd947e1c128439cd5b2ec8
moseleygj <moseleygj@gmail.com>
2017-03-30 19:26:38 -0400

    Update users.sh

--- users.sh
+++ users.sh
@@ -1,9 +1,7 @@
-
-
 #!/usr/bin/bash
-#Name:		Linux Automation project
-#purpose:	Add User Account
-#author:	Gordon Moseley
+#Name:    Linux Automation project
+#purpose: Add User Account
+#author:  Gordon Moseley
 #-------------------------------------------------------------------
 #Enter a username 
 clear
@@ -11,8 +9,8 @@ clear
 if [[ $UID != 0 ]]; then
     echo "You don't have sufficient privileges to run this script."
     exit 1
-
 fi
+
 echo "Enter a new user name, followed by [ENTER]:";
 read name
 #check if empty
@@ -21,56 +19,90 @@ user_length=$(echo -n $name | wc -c);
   echo "username \"$name\" is NOT valid[EMPTY].";
 
 elif [ "$user_length" -lt 6 ];then
-	echo "username: $name ,is too short. Try 6-32 charcters."
-	#check for invalid length
+  echo "username: $name ,is too short. Try 6-32 charcters."
+  #check for invalid length
 elif [ "$user_length" -gt 32 ];then
-	echo "username: $name , is too long."
-	#check for invalid characters
+  echo "username: $name , is too long."
+  #check for invalid characters
 elif [[ $name == *['!'@#\$%^\&*()_+]* ]]
 then
   echo "Your username contains invalid charcaters."
   else
 clear
-  echo "username  \"$name\" :	VALID";
+  echo "username  \"$name\" : VALID";
   fi
  #check if username exist already
  # $(awk -F':' '{ print $1}' );
  
 user_exists=$(id -u "$name" > /dev/null 2>&1; echo $?); 
  if [ $user_exists -eq "0" ]; then
- echo -e "username  \"$name\" :	NOT AVAILIBLE.";
+ echo -e "username  \"$name\" : NOT AVAILIBLE.";
  exit 0;
  else
- echo -e "username  \"$name\" :	AVAILIBLE.";
+ echo -e "username  \"$name\" : AVAILIBLE.";
  fi
- #exit 0
- #create user
+ 
  #ask if user wants to use a custom directory
-read -r -p "Do you want to create a custom user directory? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]];then
-echo "Directory name: ";
-read dir_name
- 	if [ -d "$dir_name" ];then 
- 	echo "$dir_name	.......	already exist!";
- 	else
- 		echo "Directory non-existant... Creating Directory ... $dir_name$name";
- 	fi
-#check if directory name is good, or currently exist
-else 
-    echo "Default directory will be used:	.......	/home/$name"
-fi
+ #[-m]
 
-
-
-
-#custom UID
-#custom GID
-#rights 
 while true; do
-    read -p "Create a custom User ID (UID) [y/n]:" yn
-    case $yn in
-        [Yy]* ) echo "Enter UID: ";read uid;break;;
-        [Nn]* ) echo "Using the default UID...";;
-        * ) echo "Please answer yes or no.";;
+    read -p "Do you want to create a custom user directory? [Y/N]:" response
+    case $response in
+        [Yy]* ) echo "Directory name: ";read dir_name;break;;
+        [Nn]* ) echo "Default directory will be used: ....... /home/$name";break;;
+        [qQ]* )  echo "Quiting";break;return 0;;
+
+        * ) echo "Please answer Yes or No.";;
     esac
 done
+
+#custom UID  [-u UID]
+while true; do
+    read -p "Create a custom User ID (UID) [Y/N]:" yn
+    case $yn in
+        [Yy]* ) echo "Enter UID: ";read uid;break;;
+        [Nn]* ) echo "Using the default UID...";break;;
+        * ) echo "Please answer Yes or No.";;
+    esac
+done
+
+#custom [GID -g group]
+while true; do
+    read -p "Create a custom Group ID (GID) [Y/N]:" groupid_ans
+    case $groupid_ans in
+        [Yy]* ) echo "The group name or number of the user's initial login group.(GID): ";read uid;break;;
+        [Nn]* ) echo "Using the default GID...";break;;
+        * ) echo "Please answer Yes or No.";;
+    esac
+done
+#Label/comment/name to identify account [-c comment]
+while true; do
+    read -p "Enter your name  to identify account (optional)[Y/N]:" full_name1
+    case $full_name1 in
+        [Yy]* ) echo "Enter Your FULL name: ";read FirstLastName;echo "Using name: $full_name, to identify your account."; break;;
+        [Nn]* ) echo "Skipping task...";break;;
+        * ) echo "Please answer Yes or No.";;
+    esac
+done
+#expiration [-e date]
+while true; do
+    read -p "Custom experation date [Y/N]:" expiration_ans
+    case $expiration_ans in
+        [Yy]* ) echo "Enter account exiration date in YYYY-MM-DD format: ";read expir_date; break;;
+        [Nn]* ) echo "Skipping task...";break;;
+        * ) echo "Please answer Yes or No.";;
+    esac
+done
+#Add use to Groups [-F, --groups group1,group2,group3]
+while true; do
+    read -p "Add user to groups[Y/N]:" groups_ans
+    case $groups_ans in
+        [Yy]* ) echo "Enter groups for user to join. Each group is separated from the next by a comma, with no
+           intervening whitespace.";read groups_add; break;;
+        [Nn]* ) echo "Skipping task...";break;;
+        * ) echo "Please answer Yes or No.";;
+    esac
+done
+
+#create password
+ 
