61e95adb1548b327f90596fd4326657131df385f
Moseley G <moseleygj@gmail.com>
2017-05-08 10:41:04 -0400

    Update website-weather-comb(clean).sh

--- website-weather-comb(clean).sh
+++ website-weather-comb(clean).sh
@@ -1,30 +1,45 @@
-#!/bin/bash
 #Gordon Moseley
-#combs weather.com site for weather quickly without the need to go on the  loging in.
-
+#Weather Comb
+#May 2017
+#You must download "lynx" to use this script.
+#
+#Sometimes the "11212:4:US"  portion will differ visit weather.com and adjust accordingly.
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#!/bin/bash
 clear;
 echo "Fetching data, please wait . . ."
 sleep 3;
 clear
+location="11212:4:US ";
 
-area=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist|
+area=$(lynx https://weather.com/weather/today/l/${location}-dump -nolist|
  sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'|
  sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|
  sed 's/as of/UPDATED: /'|sed -n 1p|sed -e 's/^[ \t]*//');
-updated=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/as of//'|sed -n 3p|sed -e 's/^[ \t]*//');
-weather=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/as of//'|sed -n 4p|sed -e 's/^[ \t]*//');
-feelsLike=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/Feels Like //'|sed -n 5p|sed -e 's/^[ \t]*//');
-highlow=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/Feels Like //'|sed -n 5p|sed -e 's/^[ \t]*//');
-UV=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/UV Index //'|sed -n 7p|sed -e 's/^[ \t]*//');
-dewPoint=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/Dew Point  //'|sed -n 11p|sed -e 's/^[ \t]*//');
-pressure=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/Pressure  //'|sed -n 12p|sed -e 's/^[ \t]*//');
-visibility=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/Visibility //'|sed -n 13p|sed -e 's/^[ \t]*//');
+weather=$(lynx https://weather.com/weather/today/l/${location}-dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/as of//'|sed -n 4p|sed -e 's/^[ \t]*//');
+highlow=$(lynx https://weather.com/weather/today/l/${location}-dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed -n 6p|sed -e 's/^[ \t]*//');
+feelsLike=$(lynx https://weather.com/weather/today/l/${location}-dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/Feels Like //'|sed -n 5p|sed -e 's/^[ \t]*//');
+humidity=$(lynx https://weather.com/weather/today/l/${location}-dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/Humidity  //'|sed -n 11p|sed -e 's/^[ \t]*//');
+UV=$(lynx https://weather.com/weather/today/l/${location}-dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/UV Index //'|sed -n 7p|sed -e 's/^[ \t]*//');
+updated=$(lynx https://weather.com/weather/today/l/${location}-dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/as of//'|sed -n 3p|sed -e 's/^[ \t]*//');
 
-echo "Location . . . . . . . . . . . . : $area";
-echo "Temperature. . . . . . . . . . . : $weather";
-echo "Updated. . . . . . . . . . . . . : $updated";
-echo "Feels like . . . . . . . . . . . : $feelsLike";
-echo "UV Index . . . . . . . . . . . . : $UV";
-echo "Dew Point. . . . . . . . . . . . : $dewPoint";
-echo "Pressure . . . . . . . . . . . . : $pressure";
-echo "Visibility . . . . . . . . . . . : $visibility";
+echo "Current Weather Statistics";
+echo "";
+echo "	Location . . . . . . . . . . . . : $area";
+echo "	Temperature. . . . . . . . . . . : $weather";
+echo "	High/Low . . . . . . . . . . . . : $highlow"
+echo "	Feels like . . . . . . . . . . . : $feelsLike";
+echo "	Humidity . . . . . . . . . . . . : $humidity";
+echo "	UV Index . . . . . . . . . . . . : $UV";
+echo "	Updated. . . . . . . . . . . . . : $updated";
+
+echo "";
