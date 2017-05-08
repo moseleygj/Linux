eed4e50340f691a957aac496dbaed9acb9e8788c
Moseley G <moseleygj@gmail.com>
2017-05-07 23:44:11 -0400

    Update website-weather-comb(clean).sh

--- website-weather-comb(clean).sh
+++ website-weather-comb(clean).sh
@@ -3,9 +3,14 @@
 #combs weather.com site for weather quickly without the need to go on the  loging in.
 
 clear;
+echo "Fetching data, please wait . . ."
 sleep 3;
+clear
 
-area=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/as of/UPDATED: /'|sed -n 1p|sed -e 's/^[ \t]*//');
+area=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist|
+ sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'|
+ sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|
+ sed 's/as of/UPDATED: /'|sed -n 1p|sed -e 's/^[ \t]*//');
 updated=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/as of//'|sed -n 3p|sed -e 's/^[ \t]*//');
 weather=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/as of//'|sed -n 4p|sed -e 's/^[ \t]*//');
 feelsLike=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/Feels Like //'|sed -n 5p|sed -e 's/^[ \t]*//');
@@ -16,10 +21,10 @@ pressure=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist|
 visibility=$(lynx https://weather.com/weather/today/l/USNY0996:1:US -dump -nolist| sed -n '/ LLC/,/Radar/p'|sed 's/© 2017 The Weather Company, LLC//'| sed 's/* Radar//'|sed -e '1,4d;13d;9d;17d;18d'|sed 's/\^°/°F/g'|sed 's/Visibility //'|sed -n 13p|sed -e 's/^[ \t]*//');
 
 echo "Location . . . . . . . . . . . . : $area";
-echo "Updated . . . . . . . . . . . .  : $updated";
-echo "Temperature . . . . . . . . . .  : $weather";
+echo "Temperature. . . . . . . . . . . : $weather";
+echo "Updated. . . . . . . . . . . . . : $updated";
 echo "Feels like . . . . . . . . . . . : $feelsLike";
 echo "UV Index . . . . . . . . . . . . : $UV";
-echo "Dew Point . . . . . . . . . . .  : $dewPoint";
+echo "Dew Point. . . . . . . . . . . . : $dewPoint";
 echo "Pressure . . . . . . . . . . . . : $pressure";
 echo "Visibility . . . . . . . . . . . : $visibility";
