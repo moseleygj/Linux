dea520d794aec67a42e5a4ac9a1e09006eb19cea
moseleygj <moseleygj@gmail.com>
2017-11-03 16:26:54 -0400

    update

---
+++ selfDestruct.sh
@@ -0,0 +1,7 @@
+#This file should be named "shred.sh".
+#create file
+touch sd.sh
+#add the remove instructions to the shred script
+echo "rm shred.sh" > sd.sh;
+#run the shred script
+./sd.sh
--- shred.sh
+++ shred.sh
@@ -16,16 +16,33 @@ mkdir $HOME_FOLDER
 #compress files
 tar cvf theSafe.tar $HOME_FOLDER
 
-#encrypt files
+#ENCRYPT THE COMPRESSED FILE
 gpg --yes --batch --passphrase=$PASSPHRASE -c $BACKUP
 
 #upload files
-#remove files
+
+HOST='ftp.host.com'
+USER='username'
+PASSWD='password'
+
+ftp -n -v $HOST << EOT
+ascii
+user $USER $PASSWD
+prompt
+cd htdocs
+ls -la
+bye
+EOT
+
+# REMOVE THE UNENCRYPTED FILE; mv BACKUP>/dev/null
 rm $BACKUP
 #remove browser history
+
 #clear last used commands
 history -c
 #remove script(self destruct)
+#This file should be named "shred.sh".
+
 touch sd.sh
 echo "rm shred.sh" > sd.sh;
 sh sd.sh
