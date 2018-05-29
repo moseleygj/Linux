7795aab7c89a2c40f44e7707d2fffb3553d9aa63
moseleygj <moseleygj@gmail.com>
2018-05-29 16:17:05 -0400

    Node List

---
+++ nodsUp.sh
@@ -0,0 +1,1 @@
+clear;sudo nmap -sn 192.168.1.0/24|grep -v "Host is up"|sed "s/Nmap scan report for //"|grep -v "Staring Nmap"|grep -v "Nmap done"|grep -v "Starting Nmap"|sed "s/MAC Address:/MAC:/g"|grep -v `hostname`
