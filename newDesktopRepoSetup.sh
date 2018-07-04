aa1d37f1da2f13823c070e725d483a4434be26b1
Gordon Moseley <moseleygj@gmail.com>
2018-07-03 22:03:11 -0400

    logic update

--- newDesktopRepoSetup.sh
+++ newDesktopRepoSetup.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 USERNAME="moseleygj"
 EMAIL="moseleygj@gmail.com"
-REPOS=(WebPages.git WebPages.git WebPages.git WebPages.git java-programming.git UI.git)
+REPOS=(WebPages.git WebPages.git java-programming.git UI.git PHP.git DB.git)
+
+#check if directory exist
+if [ -d GH ]; then
+echo "Directory GH already exist."
+else
+echo "creating directory GH..."
 mkdir GH
 cd GH
 
-if [ -e /usr/bin/dnf ] then
+if [ -e /usr/bin/dnf ]; then
 echo "DNF package-manager is present. Begining download...";
-sudo dnf install git;
-elif [ -e /usr/bin/yum ] then
+sudo dnf install git
+elif [ -e /usr/bin/yum ]; then
 echo "YUM package-manager is present. Begining download...";
 sudo yum install git
-elif [ -e /usr/bin/apt ] then
+elif [ -e /usr/bin/apt ]; then
 echo "APT package-manager is present. Begining download...";
 sudo apt-get install git
 else
-echo "Error! No package manager found."
+echo "Error No package manager found."
 fi
 
 git config --global user.name "${USERNAME}"
