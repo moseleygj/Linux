fe1f84c7b81c633c7ad94b949a7dadc132e1f5c4
moseleygj <moseleygj@gmail.com>
2018-07-07 21:17:39 -0400

    automate display settings

--- resolutionChange.sh
+++ resolutionChange.sh
@@ -4,20 +4,26 @@
 
 #find out what display to apply it to
 xrandr
+#xrandr |cut -d " " -f 1-2|grep -w "connected"
 
 echo "Enter the display you want to use:"
 read $monitor
 
-echo "Enter the desired pixels: Horiz(in Pixels) Vert(in Pixels) Hz"
-read $horizontalPixels $VerticalPixel $Hz
-#calculate VESA GTF mode lines for desired resolution (horzPix VertPixel Hz)
-gtf 2048 1152 60
+echo "Enter the desired pixels: Horiz(in Pixels) Vert(in Pixels) Hz(e.g. 60.00)"
+read $horizontalPixels $verticalPixel $Hz
 
-#add result from previous 
+#Calculate VESA GTF mode lines for desired resolution (horzPix VertPixel Hz)
+gtf 2048 1152 60
+#gtf $horizontalPixels $verticalPixels $Hz
+
+#Add result from previous 
+newRes=`gtf $horizontalPixels $verticalPixels $Hz | grep "Modeline" | cut -d " " -f 4`
 
 xrandr --newmode "2048x1152_60.00"  197.97  2048 2184 2408 2768  1152 1153 1156 1192  -HSync +Vsync
+#xrandr --newmode `gtf $horizontalPixels $verticalPixel $Hz|grep "Modeline" |cut -d " " -f 4-19`
+
 xrandr --addmode LVDS-1 "2048x1152_60.00"
+#xrandr --addmode $monitor $newRes
+
 xrandr --output LVDS-1 --mode "2048x1152_60.00"
-
-
-
+#xrandr --output $monitor --mode $newRes
