c2c1cf6e8dcc5a921225ef7d5ed5a799460e9954
moseleygj <moseleygj@gmail.com>
2018-07-19 23:52:15 -0400

    Logic update

--- resolutionChangeV1.sh
+++ resolutionChangeV1.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
+
 display=()
 count=0
+
+
+hello_world()
+{
+if [ -z $targetDisplay ];then
+	echo targetDisplay="N/A --$1"
+else
+	targetDisplay=${display[$disNum]}
+	echo targetDisplay[NOT EMPTY]=$targetDisplay
+fi
+}
+
+
 clear
 echo "Display"
 echo "--------"
@@ -13,20 +28,38 @@ echo ""
 echo "Enter display number:"
 read disNum
 
+
 if [ $disNum = 1 ]; then
-echo "option 1 selected"
+	echo "option 1: selected"
+	targetDisplay=${display[$disNum]}
+	echo "display is ${display[$disNum]}"
+	hello_world $targetDisplay
+
 elif [ $disNum = 2 ]; then
-echo "option 2 selected"
+	echo "option 2: selected"
+	hello_world $1
+
 elif [ $disNum = 3 ]; then
-echo "option 3 selected"
+	echo "option 3: selected"
+	targetDisplay=${display[$disNum]}
+
 elif [ $disNum = 4 ]; then
-echo "option 4 selected"
+	echo "option 4: selected"
+	targetDisplay=${display[$disNum]}
+
 elif [ $disNum = 5 ]; then
-echo "option 5 selected"
+	echo "option 5: selected"
+	targetDisplay=${display[$disNum]}
+
 elif [ $disNum = 6 ]; then
-echo "option 6 selected"
+	targetDisplay=${display[$disNum]}
+	echo "option: 6 selected"
+	targetDisplay=${display[$disNum]}
 else
 echo "Error invalid option or too many monitors(>6)!"
+
 fi
 
+
+#add an option for 30hz-60hz
 
