8f3fb5e25f2e4fcd886b55cee061586a24789e27
moseleygj <moseleygj@gmail.com>
2019-04-10 17:15:50 -0400

    comepleted: automate youtube music download from terminal

---
+++ autoDownloadMP3/mp3.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+#lynx -dump 'https://www.youtube.com/results?search_query=afroman+because+i+got+high+mp3'|grep watch|grep https|awk '{print $2}'|head -n 1
+dump="lynx -dump 'https://www.youtube.com/results?search_query="
+link="| grep watch | grep https | awk '{print \$2}'| head -n 1"
+div="------------------------------------------------------"
+
+p1="lynx -dump "
+p2=\'
+p3="https://www.youtube.com/results?search_query="
+
+yd="youtube-dl -x -i --extract-audio --audio-format mp3 --yes-playlist --batch-file song.txt"
+n=""
+
+clear
+echo "Enter song name and artist:";
+read song;
+for i in $song
+do
+queryString+="$i+";
+done
+
+new=$(echo -n $queryString | head -c -1)
+newer="$new+audio"
+echo "SEARCHING FOR: $song . . ."
+echo ""
+comp=$p1$p2$p3$newer$p2$link
+eval $comp > song.txt
+eval $yd
+echo ""
+
+echo "======================"
+echo "  DOWNLOAD COMPLETE!"
+echo "======================"
+
+#echo $(date +%b_%d_%Y);
