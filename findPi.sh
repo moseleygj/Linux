be9b8b5b0ad7e46590fd8e66cf9c54acfd5b42c4
Gordon Moseley <moseleygj@gmail.com>
2019-12-27 20:39:11 -0500

    Find My Pi on Network

---
+++ findPi.sh
@@ -0,0 +1,1 @@
+sudo nmap -sP 192.168.1.0/24|awk '/^Nmap/{ip=$NF}/00:C0:CA/{print ip}';sudo nmap -sP 192.168.1.0/24|awk '/^Nmap/{ip=$NF}/6A:4C:BF/{print ip}';sudo nmap -sP 192.168.1.0/24|awk '/^Nmap/{ip=$NF}/BE:47:F2/{print ip}';
