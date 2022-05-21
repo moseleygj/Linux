739a2e2dd8362a79d8289098776f17badb309075
moseleygj <moseleygj@gmail.com>
2022-05-21 15:58:04 -0400

    cleanup code and comment

--- webpRename.sh
+++ webpRename.sh
@@ -3,6 +3,9 @@ for x in $(ls *.webp)
 do
  echo Converting: $x
  mv $x ${x%.webp}.jpg
-echo "Removing old file..."
+ echo "Removing old file..."
  rm $x
 done
+
+echo "Done!"
+#rename and remove old files
