e2120ba8d9d575891bb4ffc6c5d03eab3bdf8efb
moseleygj <moseleygj@gmail.com>
2022-06-03 17:22:17 -0400

    cleanup code

--- moveByFileExt/cleanup.sh
+++ moveByFileExt/cleanup.sh
@@ -16,53 +16,51 @@ gif="/home/`whoami`/Pictures/GIFs"
 ##Remove the single has "#" from the beginging of lines to make script active
 ##pictures
 if [ -d "$pictures" ]; then
-echo "Directory: $pictures exist";
+  echo "Directory: $pictures exist.";
 #mv *.{jpg,JPG,png,PNG,jpeg,JPEG,bmp,BMP,ico,ICO,gif,GIF,}tiff,TIFF $pictures
 else
-echo "Directory: $pictures directory doesn't exist."
-echo
-echo "Creating directory \"Pictures\"...";
-mkdir $pictures
+  echo "Directory: $pictures directory doesn't exist."
+  echo
+  echo "Creating directory \"Pictures\"...";
+  mkdir $pictures
 #mv *.{jpg,JPG,png,PNG,jpeg,JPEG,bmp,BMP,ico,ICO,tiff,TIFF,gif,GIF} $pictures
 fi
 
 
 ##documents
 if [ -d "$douments" ]; then
-echo "Directory: $documents exist";
+  echo "Directory: $documents exist";
 #mv *.{txt,TXT,doc,DOC,docx,DOCX,rtf,RTF,pdf,PDF} $documents
 else
-echo "Directory: $documents directory doesn't exist."
-echo
-echo "Creating directory \"Documents\"...";
-mkdir $documents
+  echo "Directory: $documents doesn't exist."
+  echo
+  echo "Creating directory \"Documents\"...";
+  mkdir $documents
 #mv *.{txt,TXT,doc,DOC,docx,DOCX,rtf,RTF,pdf,PDF} $documents
 fi
 
 
 ##video
 if [ -d "$video" ]; then
-echo "Directory: $video exist";
+  echo "Directory: $video exist";
 #mv *.{mp4,MP4,mpeg,MPEG,mov,MOV,flv,FLV,mpg,MPG} $videos
 else
-echo "Directory: $video directory doesn't exist."
-echo
-echo "Creating directory \"Videos\"...";
-mkdir $video
+  echo "Directory: $video directory doesn't exist."
+  echo
+  echo "Creating directory \"Videos\"...";
+  mkdir $video
 #mv *.{mp4,MP4,mpeg,MPEG,mov,MOV,flv,FLV,mpg,MPG} $videos
 fi
 
 
 ##music
 if [ -d "$music" ]; then
-echo "Directory: $music exist";
+  echo "Directory: $music exist";
 #mv *.{MP3,mp3,wav,WAV,aiff,AIFF,au,AU,acc,ACC,wma,WMA,MIDI,midi,ogg,OGG,m4a,M4A,ape,APE,WV,wv,alac,ALAC,flac,FLAC,tta,TTA} $music
 else
-echo "Directory: $music directory doesn't exist."
-echo
-echo "Creating directory \"Music\"...";
-mkdir $music
+  echo "Directory: $music directory doesn't exist."
+  echo
+  echo "Creating directory \"Music\"...";
+  mkdir $music
 #mv *.{MP3,mp3,wav,WAV,aiff,AIFF,au,AU,acc,ACC,wma,WMA,MIDI,midi,ogg,OGG,m4a,M4A,ape,APE,WV,wv,alac,ALAC,flac,FLAC,tta,TTA} $music
 fi
-
-
